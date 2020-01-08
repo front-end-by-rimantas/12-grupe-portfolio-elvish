4dcc858e3a136a6370face8f1c2ffaffe9fa9fb9
Aivaras <lukauskasaivaras@gmail.com>
2020-01-08 12:41:29 +0100

    javascript comments

--- js/data.js
+++ js/data.js
@@ -1,2 +1,28 @@
 "use strict";
 
+// header start //
+// header end //
+
+// hero start //
+// hero end //
+
+// progress bar start //
+// progress bar end //
+
+// our services start //
+// our services end //
+
+// achievements start //
+// achievements end //
+
+// education & experience start //
+// education & experience end //
+
+// gallery start //
+// gallery end //
+
+// partners start //
+// partners end //
+
+// blog start //
+// blog end //
