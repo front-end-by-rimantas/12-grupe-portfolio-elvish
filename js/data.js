5338abdb10c9a5a120ea77742feb2ffbf955351e
Aivaras <lukauskasaivaras@gmail.com>
2020-01-10 10:28:29 +0100

    achievements done

--- js/data.js
+++ js/data.js
@@ -9,70 +9,70 @@
 // progress bar start //
 const skills = [
     {
-    tittle: 'Development',
-    value: 80
+        tittle: 'Development',
+        value: 80
     },
     {
-    tittle: 'Html',
-    value: 96
+        tittle: 'Html',
+        value: 96
     },
     {
-    tittle: 'Photoshop',
-    value: 88
+        tittle: 'Photoshop',
+        value: 88
     },
     {
-    tittle: 'Css',
-    value: 70
+        tittle: 'Css',
+        value: 70
     },
     {
-    tittle: 'Wordpress',
-    value: 59
+        tittle: 'Wordpress',
+        value: 59
     },
     {
-    tittle: 'Asp.net',
-    value: 58
+        tittle: 'Asp.net',
+        value: 58
     }
 ]
 // progress bar end //
 
 // our services start //
 const services = [
-    { 
+    {
         icon: 'fa-address-book-o',
         heading: 'Graphic Design',
         par: 'The standard chunk of Lorem Ipsum used since the is reproduced below for those interested',
 
 
     },
-    { 
+    {
         icon: 'fa-address-card-o',
         heading: 'Unlimited Color',
         par: 'The standard chunk of Lorem Ipsum used since the is reproduced below for those interested',
 
 
     },
-    { 
+    {
         icon: 'fa-calendar-plus-o',
         heading: 'Media Marketing',
         par: 'The standard chunk of Lorem Ipsum used since the is reproduced below for those interested',
 
 
     },
-    { 
+    {
         icon: 'fa-wifi',
         heading: 'Unlimited Wifi',
         par: 'The standard chunk of Lorem Ipsum used since the is reproduced below for those interested',
 
 
     },
-    { 
+    {
         icon: 'fa-television',
         heading: 'Responsive Design',
         par: 'The standard chunk of Lorem Ipsum used since the is reproduced below for those interested',
-        
+
 
     },
-    { 
+    {
         icon: 'fa-pencil-square-o',
         heading: 'Easy to customize',
         par: 'The standard chunk of Lorem Ipsum used since the is reproduced below for those interested',
@@ -82,6 +82,28 @@ const services = [
 // our services end //
 
 // achievements start //
+const achievements = [
+    {
+        icon: 'fa-clock-o',
+        amount: 654,
+        title: 'Working Hours'
+    },
+    {
+        icon: 'fa-gift',
+        amount: 6400,
+        title: 'Completed Projects'
+    },
+    {
+        icon: 'fa-user',
+        amount: 2389,
+        title: 'No. of Clients'
+    },
+    {
+        icon: 'fa-users',
+        amount: 653,
+        title: 'Team Members'
+    }
+]
 // achievements end //
 
 // education & experience start //
